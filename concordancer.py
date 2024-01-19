0b475898b4ff47d8e9ce122a875308b71c0a601f
Mikhail Koninin <mkoninin@gmail.com>
2024-01-19 17:10:37 +0700

    Update concordancer.py

--- concordancer.py
+++ concordancer.py
@@ -17,4 +17,56 @@ class concordancer:
     def word(word: str) -> list[tuple]:
         pass
 
-#%%
+# %%
+
+text = 'Мама мыла раму. Рама мыла маму.'
+
+
+# %%
+from razdel import sentenize
+from razdel import tokenize
+
+# %%
+sentences = list(sentenize(text))
+
+for sentence in sentences:
+    print(list(tokenize(sentence.text)))
+# %%
+sentence.text
+# %%
+
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+    
+    PER,
+    NamesExtractor,
+
+    Doc
+)
+segmenter = Segmenter()
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+morph_vocab = MorphVocab()
+
+doc = Doc(text)
+# %%
+doc.segment(segmenter)
+
+doc.tag_morph(morph_tagger)
+doc.parse_syntax(syntax_parser)
+for token in doc.tokens:
+    token.lemmatize(morph_vocab)
+print(doc.tokens)
+# %%
+
+for token in doc.tokens:
+    if token.lemma == 'мыть':
+        print(token) 
+# %%
